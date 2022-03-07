32c107135636e089bc939bf5bcac61c5ab088b82
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 19:04:22 -0300

    ex6

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -1,13 +1,24 @@
-const a1 = 90;
-const a2 = 200;
-const a3 = 46;
+peca = "REI"
 
-if (a1 < 0 || a2 < 0 || a3 < 0) {
-    console.log("insira um angulo valido");
-}
-else if (a1 + a2 + a3 === 180){
-    console.log(true);
-}
-else {
-    console.log(false);
+switch(peca.toLowerCase()) {
+    case "torre" :
+        console.log("A torre movimenta-se em direção reta pelas colunas ou fileiras.")
+        break;
+    case "cavalo" :
+        console.log("O cavalo movimenta-se sempre em L,ou seja,duas casas para frente e uma para a esquerda ou direita. O cavalo é a única peça que pode pular sobre as outras, tanto as suas quanto as adversárias.");
+        break;
+    case "bispo" :
+        console.log("O bispo move-se na diagonal, quantas casas quiser.");
+        break;
+    case "rainha" :
+        console.log("A rainha pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente.");
+        break;
+    case "rei" :
+        console.log("O rei pode se mover para qualquer casa adjacente. Assim, ele pode mover-se uma casa em qualquer direção: horizontalmente, verticalmente ou diagonalmente.");
+        break;
+    case "peao" :
+        console.log("De maneira geral, os peões se movem somente para frente, uma casa por vez. Um exceção é a primeira vez que um peão é movido, quando ele pode se mover duas casas.");
+        break;
+    default:
+        console.log("Insira uma peca valida");
 }
