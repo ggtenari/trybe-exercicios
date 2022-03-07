f1ed294937076001f1951620b378cf3e4e25f198
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 19:10:38 -0300

    ex7

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -1,24 +1,19 @@
-peca = "REI"
-
-switch(peca.toLowerCase()) {
-    case "torre" :
-        console.log("A torre movimenta-se em direção reta pelas colunas ou fileiras.")
-        break;
-    case "cavalo" :
-        console.log("O cavalo movimenta-se sempre em L,ou seja,duas casas para frente e uma para a esquerda ou direita. O cavalo é a única peça que pode pular sobre as outras, tanto as suas quanto as adversárias.");
-        break;
-    case "bispo" :
-        console.log("O bispo move-se na diagonal, quantas casas quiser.");
-        break;
-    case "rainha" :
-        console.log("A rainha pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente.");
-        break;
-    case "rei" :
-        console.log("O rei pode se mover para qualquer casa adjacente. Assim, ele pode mover-se uma casa em qualquer direção: horizontalmente, verticalmente ou diagonalmente.");
-        break;
-    case "peao" :
-        console.log("De maneira geral, os peões se movem somente para frente, uma casa por vez. Um exceção é a primeira vez que um peão é movido, quando ele pode se mover duas casas.");
-        break;
-    default:
-        console.log("Insira uma peca valida");
+nota = 60;
+if (nota >= 90) {
+    console.log("A");
+}
+else if (nota >= 80) {
+    console.log("B");
+}
+else if (nota >= 70) {
+    console.log("C");
+}
+else if (nota >= 60) {
+    console.log("D");
+}
+else if (nota >= 50) {
+    console.log("E");
+}
+else if (nota < 50) {
+    console.log("F");
 }
