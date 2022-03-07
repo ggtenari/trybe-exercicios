662e144b5663c520a7008c0f7ed33d60a8a148ae
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 18:16:43 -0300

    ex1

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -1,4 +1,14 @@
-const base = 5
-let height = 8
-const area = base * height
-console.log(area)
+let a = 9;
+let b = 2;
+
+let soma = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
