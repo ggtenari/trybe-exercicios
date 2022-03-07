f075409eed7fe6899e5ea82c02d8e395f62c80f5
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 19:15:49 -0300

    ex9

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -1,10 +1,10 @@
 const a = 3
-const b = 5
-const c = 9
+const b = 4
+const c = 2
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 ===0) {
-    console.log(true);
+if (a % 2 === 0 && b % 2 === 0 && c % 2 ===0) {
+    console.log(false);
 }
 else {
-    console.log(false);
+    console.log(true);
 }
