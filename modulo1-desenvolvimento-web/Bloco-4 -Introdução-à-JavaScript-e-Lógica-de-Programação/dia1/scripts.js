c4fac7e9748fb8c1394c393e885e9eddc67aab25
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 18:44:14 -0300

    ex5-fixed

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -2,7 +2,7 @@ const a1 = 90;
 const a2 = 200;
 const a3 = 46;
 
-if (a1 < 0 || a1 > 180 || a2 < 0 || a2> 180 || a3 < 0 || a3 >180) {
+if (a1 < 0 || a2 < 0 || a3 < 0) {
     console.log("insira um angulo valido");
 }
 else if (a1 + a2 + a3 === 180){
