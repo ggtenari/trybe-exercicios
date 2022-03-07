4c0855f5a000f6e8631054a97c6c01c239cefb2f
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 20:33:45 -0300

    ex11

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -1,6 +1,29 @@
-const custo = 20
-const venda = 35
+let salario = 3000;
 
-let lucro = (venda - (custo * 1.2)) * 1000
+if (salario < 1566.94) {
+    salario = salario * 0.92
+}
+else if (salario <= 2594.92) {
+    salario = salario * 0.91
+}
+else if (salario <= 5189.82) {
+    salario = salario * 0.89
+}
+else if (salario > 5189.82) {
+    salario = salario - 570.88
+}
 
-console.log(lucro);
+if (salario > 4664.68) {
+        salario = salario - ((salario * 0.275) - 869.36)
+    }
+else if (salario > 3751.06) {
+    salario = salario - ((salario * 0.225) - 636.13)
+}
+else if (salario > 2826.66) {
+    salario = salario - ((salario * 0.15) - 354.80)
+}
+else if (salario > 1903.99) {
+    salario = salario - ((salario * 0.075) - 142.80)
+}
+
+console.log(salario);
