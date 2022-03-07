513f25cd937be428d10a57e1bd317c92f12533bf
Gabriel Tenari <ggtenari@gmail.com>
2022-03-07 19:13:39 -0300

    ex8

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia1/scripts.js
@@ -1,19 +1,10 @@
-nota = 60;
-if (nota >= 90) {
-    console.log("A");
+const a = 3
+const b = 5
+const c = 9
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 ===0) {
+    console.log(true);
 }
-else if (nota >= 80) {
-    console.log("B");
-}
-else if (nota >= 70) {
-    console.log("C");
-}
-else if (nota >= 60) {
-    console.log("D");
-}
-else if (nota >= 50) {
-    console.log("E");
-}
-else if (nota < 50) {
-    console.log("F");
+else {
+    console.log(false);
 }
