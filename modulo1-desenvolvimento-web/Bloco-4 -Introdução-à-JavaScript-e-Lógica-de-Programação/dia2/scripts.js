491860c6de92f907ab4389adc80c18e31980869a
Gabriel Tenari <ggtenari@gmail.com>
2022-03-08 16:11:03 -0300

    ex8

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia2/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia2/scripts.js
@@ -1,12 +1,6 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = []
+let numbers = []
 
-for (numero of numbers) {
-    if (menor.length === 0) {
-        menor.push(numero)
-    }
-    else if (numero < menor) {
-        menor[0] = numero
-    }   
+for (let index = 1; index <= 25; index++) {
+numbers.push(index)
 }
-console.log(menor);
+console.log(numbers);
