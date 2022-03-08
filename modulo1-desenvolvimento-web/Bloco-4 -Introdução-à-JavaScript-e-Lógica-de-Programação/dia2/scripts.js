564c50a34dd547605ad4eac42d2f8b5be3ad9c97
Gabriel Tenari <ggtenari@gmail.com>
2022-03-08 15:53:44 -0300

    ex5

--- modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia2/scripts.js
+++ modulo1-desenvolvimento-web/Bloco-4 -Introdução-à-JavaScript-e-Lógica-de-Programação/dia2/scripts.js
@@ -1,17 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maior = 0
 
 for (let numero of numbers) {
-    soma = soma + numero
+    if (numero > maior) {
+        maior = numero
+    }
 }
-
-soma = soma / numbers.length
-
-if (soma > 20) {
-    console.log("Soma maior que vinte");
-}
-else {
-    console.log("Soma menor ou igual a vinte");
-}
-
-console.log(soma);
+console.log(maior);
