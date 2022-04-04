5978a212e5a07e327eff03fedcce7882a0ec75d0
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-04-04 19:22:13 -0300

    pt2

--- modulo1-desenvolvimento-web/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/dia2/script.js
+++ modulo1-desenvolvimento-web/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/dia2/script.js
@@ -1,10 +1,52 @@
-const coolestTvShow = {
-    name: "BoJack Horseman",
-    genre: "adult animation",
-    createdBy: "Raphael Bob-Waksberg",
-    favoriteCharacter: "Princess Carolyn",
-    quote: "Princess Carolyn always lands on her feet.",
-    seasons: 6,
-  };
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
 
-  console.log(Object.keys(coolestTvShow));
+// const customerInfo = (order) => {
+//   // Adicione abaixo as informações necessárias.
+//   return `Ola ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, No: ${order.address.number}, AP: ${order.address.apartment}`
+// }
+
+// console.log(customerInfo(order));
+
+const orderModifier = (order) => {
+  order.name = "Luiz Silva"
+  order.payment.total = 50
+  return `Ola ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)[0]}, ${Object.keys(order.order.pizza)[1]} e ${order.order.drinks.coke.type} eh R$ ${order.payment.total},00.`
+
+}
+
+orderModifier(order);
+console.log(orderModifier(order));
