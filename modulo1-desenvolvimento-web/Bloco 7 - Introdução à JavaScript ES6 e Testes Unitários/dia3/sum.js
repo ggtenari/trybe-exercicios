3776638c570b97f97c8f69fbb28e86a8b3d36810
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-04-07 15:01:47 -0300

    ex

--- modulo1-desenvolvimento-web/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/dia3/sum.js
+++ modulo1-desenvolvimento-web/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/dia3/sum.js
@@ -1,9 +1,67 @@
 function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("parameters must be numbers");
   }
 
   return a + b;
 }
 
-module.exports = sum
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+function myFizzBuzz(num) {
+  if (typeof num !== "number") return false;
+  if (num % 3 === 0 && num % 5 === 0) return "fizzbuzz";
+  if (num % 3 === 0) return "fizz";
+  if (num % 5 === 0) return "buzz";
+  return num;
+}
+
+function encode(string) {
+  let newString = "";
+  for (letter of string) {
+    if (letter === "a") {
+      newString += "1";
+    } else if (letter === "e") {
+      newString += "2";
+    } else if (letter === "i") {
+      newString += "3";
+    } else if (letter === "o") {
+      newString += "4";
+    } else if (letter === "u") {
+      newString += "5";
+    } else {
+      newString += letter;
+    }
+  }
+  return newString;
+}
+
+function decode(string) {
+  let newString = "";
+  for (letter of string) {
+    if (letter === "1") {
+      newString += "a";
+    } else if (letter === "2") {
+      newString += "e";
+    } else if (letter === "3") {
+      newString += "i";
+    } else if (letter === "4") {
+      newString += "o";
+    } else if (letter === "5") {
+      newString += "u";
+    } else {
+      newString += letter;
+    }
+  }
+  return newString;
+}
+
+module.exports = { sum, myRemove, myFizzBuzz, encode, decode};
--- modulo1-desenvolvimento-web/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/dia3/sum.test.js
+++ modulo1-desenvolvimento-web/Bloco 7 - Introdução à JavaScript ES6 e Testes Unitários/dia3/sum.test.js
@@ -1,18 +1,57 @@
-const { expect, test } = require('@jest/globals');
-const sum = require('./sum');
+const {sum, myRemove, myFizzBuzz, encode, decode} = require('./sum');
 
-describe('primeira funcao', () => {
-  test('retorno de sum(4, 5) é 9', () => {
-    expect(sum(4, 5)).toBe(9)
+
+// describe('primeira funcao', () => {
+//   test('retorno de sum(4, 5) é 9', () => {
+//     expect(sum(4, 5)).toBe(9)
+//   })
+//   test('retorno de sum(0, 0) é 0', () => {
+//     expect(sum(0, 0)).toBe(0)
+//   })
+//   test('a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+//     expect(() => {sum(4, '5')}).toThrow()
+//   })
+//   test('a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+//     expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers'))
+//   })
+// })
+
+// describe('segunda funcao', () => {
+//   test('a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+//     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+//   })
+//   test('a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+//     expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
+//   })
+//   test('a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+//     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+//   })
+// })
+
+// describe('terceira funcao', () => {
+//   test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+//     expect(myFizzBuzz(15)).toBe('fizzbuzz')
+//   })
+//   test('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+//     expect(myFizzBuzz(9)).toBe('fizz')
+//   })
+//   test('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+//     expect(myFizzBuzz(10)).toBe('buzz')
+//   })
+//   test('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+//     expect(myFizzBuzz(7)).toBe(7)
+//   })
+//   test('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+//     expect(myFizzBuzz('string')).toBeFalsy()
+//   })
+// })
+
+describe('funcao encode, decode', () => {
+  test('Teste se encode e decode são funções', () => {
+    expect(typeof(decode)).toBe('function')
+    expect(typeof(encode)).toBe('function')
   })
-  test('retorno de sum(0, 0) é 0', () => {
-    expect(sum(0, 0)).toBe(0)
-  })
-  test('a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
-    expect(() => {sum(4, '5')}).toThrow()
-  })
-  test('a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
-    expect(() => {sum(4, '5')}).toThrowError(new Error('parameters must be numbers'))
+  test('Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+    expect(encode('a', 'e', 'i', 'o', 'u')).toBe('1', '2', '3', '4', '5')
   })
 })
-
