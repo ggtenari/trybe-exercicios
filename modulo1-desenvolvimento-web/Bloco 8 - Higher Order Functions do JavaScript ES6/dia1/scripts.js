109912e7bf4d76ef208ba58204a39953a95483dd
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-04-07 16:12:06 -0300

    ex2

--- modulo1-desenvolvimento-web/Bloco 8 - Higher Order Functions do JavaScript ES6/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco 8 - Higher Order Functions do JavaScript ES6/dia1/scripts.js
@@ -1,23 +1,12 @@
-const cadastro = (nome) => {
-  let nomeSeparado = nome.split(' ')
-  let email = nomeSeparado[0]
-  for (i = 1; i < nomeSeparado.length; i += 1){
-    email += `_${nomeSeparado[i]}`
-  }
-  const funcionario = {
-    nomeCompleto: nome,
-    email: email + `@trybe.com`
-  }
-  return funcionario
+const check = (aposta, funcao) => {
+  let numeroSorteado = Math.floor(Math.random() * 5 + 1)
+  let resultado = funcao(aposta, numeroSorteado)
+  return resultado
 }
 
-const newEmployees = (funcao) => {
-  const employees = {
-    id1: funcao('Pedro Guerra'),
-    id2: funcao('Luiza Drumond'),
-    id3: funcao('Carla Paiva'),
-  }
-  return employees;
-};
+const checagem = (apostado, sorteado) => {
+  apostado === sorteado ? returno = 'Parabéns você ganhou' : returno = 'Tente novamente'
+  return returno
+}
 
-console.log(newEmployees(cadastro));
+console.log(check(1, checagem));
