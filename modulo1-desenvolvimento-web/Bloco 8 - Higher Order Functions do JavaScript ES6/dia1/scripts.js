e3daa29d86b819f8da8ec439d24b8f5a3e967ef7
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-04-07 16:31:14 -0300

    ex3

--- modulo1-desenvolvimento-web/Bloco 8 - Higher Order Functions do JavaScript ES6/dia1/scripts.js
+++ modulo1-desenvolvimento-web/Bloco 8 - Higher Order Functions do JavaScript ES6/dia1/scripts.js
@@ -1,12 +1,23 @@
-const check = (aposta, funcao) => {
-  let numeroSorteado = Math.floor(Math.random() * 5 + 1)
-  let resultado = funcao(aposta, numeroSorteado)
-  return resultado
+const contagem = (gabarito, aluno) => {
+  let acertos = 0
+  for (i = 0; i < gabarito.length; i += 1) {
+    if (gabarito[i] === aluno[i]) {
+      acertos += 1
+    } else if (aluno[i] === 'N.A') {
+      acertos += 0
+    } else {
+      acertos -= 0.5
+    }
+  }
+  return acertos
+}
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const HOF = (listaGabaritos, listaResposta, funcaoCheca) => {
+  
+  let contagemAcertos = funcaoCheca(listaGabaritos, listaResposta)
+  return contagemAcertos
 }
 
-const checagem = (apostado, sorteado) => {
-  apostado === sorteado ? returno = 'Parabéns você ganhou' : returno = 'Tente novamente'
-  return returno
-}
-
-console.log(check(1, checagem));
+console.log(HOF(RIGHT_ANSWERS, STUDENT_ANSWERS, contagem));
