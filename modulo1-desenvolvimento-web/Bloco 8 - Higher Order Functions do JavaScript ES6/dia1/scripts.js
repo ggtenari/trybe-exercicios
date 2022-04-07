5508848ee8dcdd52647174ac0f6c5f3d651a96c3
Gabriel Tenari <gabriel.tenari.ismart@gmail.com>
2022-04-07 15:30:46 -0300

    ex de aula

---
+++ modulo1-desenvolvimento-web/Bloco 8 - Higher Order Functions do JavaScript ES6/dia1/scripts.js
@@ -0,0 +1,12 @@
+const acordando = () => 'acordando'
+const cafe = () => `Bora tomar café!!`
+const dormir = () => `Partiu dormir!!`
+
+const doingThings = (func) => {
+  console.log(func());
+}
+
+doingThings(acordando)
+doingThings(cafe)
+doingThings(dormir)
+
